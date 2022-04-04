508c092d719360c240c47540660944153a9a91a3
udit.saurav <udit.saurav@olacabs.com>
2022-04-04 16:34:43 +0530

    Implementation: phone otp verification

---
+++ app/src/main/java/com/example/splitter/RegisterUser2.java
@@ -0,0 +1,134 @@
+package com.example.splitter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class RegisterUser2 extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText edtPhone, edtOTP;
+    private Button verifyOTPBtn, generateOTPBtn;
+    private String verificationId;
+    // callback method is called on Phone auth provider.
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        // below method is used when OTP is sent from Firebase
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            // when we receive the OTP it contains a unique id which we are storing in our string which we have already created.
+            verificationId = s;
+        }
+
+        // this method is called when user receive OTP from Firebase.
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            // below line is used for getting OTP code which is sent in phone auth credentials.
+            final String code = phoneAuthCredential.getSmsCode();
+            if (code != null) {
+                edtOTP.setText(code);
+                verifyCode(code);
+            }
+        }
+
+        // this method is called when firebase doesn't sends our OTP code due to any error or issue.
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            // displaying error message with firebase exception.
+            Toast.makeText(RegisterUser2.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user2);
+
+        mAuth = FirebaseAuth.getInstance();
+        edtPhone = findViewById(R.id.idEdtPhoneNumber);
+        edtOTP = findViewById(R.id.idEdtOtp);
+        verifyOTPBtn = findViewById(R.id.idBtnVerify);
+        generateOTPBtn = findViewById(R.id.idBtnGetOtp);
+
+        // setting onclick listener for generate OTP button.
+        generateOTPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(edtPhone.getText().toString())) {
+                    Toast.makeText(RegisterUser2.this, "Please enter a valid phone number.", Toast.LENGTH_SHORT).show();
+                } else {
+                    String phone = "+91" + edtPhone.getText().toString();
+                    sendVerificationCode(phone);
+                }
+            }
+        });
+
+        // initializing on click listener for verify otp button
+        verifyOTPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // validating if the OTP text field is empty or not.
+                if (TextUtils.isEmpty(edtOTP.getText().toString())) {
+                    // if the OTP text field is empty display a message to user to enter OTP
+                    Toast.makeText(RegisterUser2.this, "Please enter OTP", Toast.LENGTH_SHORT).show();
+                } else {
+                    // if OTP field is not empty calling method to verify the OTP.
+                    verifyCode(edtOTP.getText().toString());
+                }
+            }
+        });
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        // inside this method we are checking if the code entered is correct or not.
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent i = new Intent(RegisterUser2.this, MainActivity.class);
+                            startActivity(i);
+                            finish();
+                        } else {
+                            Toast.makeText(RegisterUser2.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode(String number) {
+        // this method is used for getting OTP on user phone number.
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(number)         // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallBack)         // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    // below method is use to verify code from Firebase.
+    private void verifyCode(String code) {
+        // below line is used for getting getting credentials from our verification id and code.
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithCredential(credential);
+    }
+}
